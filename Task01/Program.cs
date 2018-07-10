0eb560d028b70d64e63cb26d6708b499bfc6a0cd
Alexey Kugachev <alexey_kugachev@epam.com>
2018-07-10 13:37:26 +0300

    Доделано задание 1

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -10,6 +10,40 @@ namespace Task01
     {
         static void Main(string[] args)
         {
+            Unit unit = new Unit();
+            DinamicArray<Unit> arr = new DinamicArray<Unit>(3);
+            arr.Add(unit);
+            arr.Add(unit);
+            Console.WriteLine(arr.ToString()); //вывод неполного массива
+            arr.Add(unit);
+            Console.WriteLine(arr.ToString()); //вывод полного массива
+            arr.Add(unit);
+            Console.WriteLine(arr.ToString()); //вывод массива после расширения из-за недостаточной вместимости
+
+            Unit[] arr2 = new Unit[] { new Unit(), new Unit(), new Unit(), new Unit(), new Unit(), new Unit(), new Unit(), new Unit() };
+            arr.AddRange(arr2);
+            Console.WriteLine(arr.ToString()); //вывод массива после добавления нового массива и расширения из-за недостаточной вместимости
+
+            arr.Remove(2);
+            Console.WriteLine(arr.ToString()); //вывод после удаления элемента массива со сдвигом ячеек
+
+            arr.Insert(2, default(Unit));
+            Console.WriteLine(arr.ToString()); //вывод после вставки элемента массива со сдвигом ячеек
+
+            arr.Set(3, default(Unit));
+            Console.WriteLine(arr.ToString());//вывод после изменения элемента массива по индексу
+
+            Console.WriteLine("Элемент массива: " + arr.Get(4)); //получение элемента массива по индексу
+
+            Console.ReadKey();
+        }
+    }
+
+    class Unit
+    {
+        public Unit()
+        {
+
         }
     }
 }
